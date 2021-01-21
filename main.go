cc2143ab574d6ebd5cfdfebe3568d029a3ae4470
Sebastian Schweiger <SeSc838@users.noreply.github.com>
2021-01-21 18:42:35 +0100

    Refactored main.go
    Moved determinition of project name to own function
    Moved check for flags to own function
    Moved creation of main.go file to own function
    Moved initialization of go module to own function

--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 	"os/exec"
 )
 
-func main() {
-	// Get Commandline input and determine project name
-	args := os.Args
-	var name string
-	var module bool = false
+func getProjectName(args []string) string {
 	if len(args) == 1 {
-		name = "NewGoProject"
-	} else {
-		name = args[1]
+		return "NewGoProject"
 	}
+	return args[1]
+}
+
+func checkFlags(args []string) (module bool) {
+	module = false
+
 	// Check for additional flags
 	if len(args) > 2 {
 		for i := 2; i < len(args); i++ {
@@ -26,8 +26,45 @@ func main() {
 				module = true
 			}
 		}
+	}
+	return module
+}
+
+func createMainGo(pathName string) {
+	// Check if main.go already exists -> this should never happen
+	info, _ := os.Stat(pathName)
+
+	// Create main.go if not existing
+	if info == nil {
+		errMain := ioutil.WriteFile(pathName, []byte("package main\n\nfunc main(){\n\n}"), 0755)
+		if errMain != nil {
+			log.Fatal(errMain)
+			return
+		}
+	}
+}
+
+func generateModule(module bool, name string) {
+	// If module flag is set
+	if module {
+
+		os.Chdir("./" + name)
+
+		cmd := exec.Command("go", "mod", "init")
+
+		if _, err := cmd.Output(); err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
 
 	}
+}
+
+func main() {
+	// Get Commandline input and determine project name
+	args := os.Args
+	name := getProjectName(args)
+	module := checkFlags(args)
 
 	// Check if project folder already exists
 	infoDir, _ := os.Stat(name)
@@ -42,34 +79,10 @@ func main() {
 
 		mainPath := name + "/main.go"
 
-		// Check if main.go already exists -> this should never happen
-		info, _ := os.Stat(mainPath)
-
-		// Create main.go if not existing
-		if info == nil {
-			err2 := ioutil.WriteFile(mainPath, []byte("package main\n\nfunc main(){\n\n}"), 0755)
-			if err2 != nil {
-				log.Fatal(err2)
-				return
-			}
-		}
-
-		// If module flag is set
-		if module {
-
-			os.Chdir("./" + name)
-
-			cmd := exec.Command("go", "mod", "init")
-
-			if _, err := cmd.Output(); err != nil {
-				fmt.Println("Error: ", err)
-				return
-			}
-
-		}
+		createMainGo(mainPath)
+		generateModule(module, name)
 
 		fmt.Printf("Project %s was created.\n", name)
-
 	} else {
 		fmt.Printf("Project/Folder %s already exists.\n", name)
 	}

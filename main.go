a0f990f6487a96825b226a100dad7f73f1fa9845
Sebastian Schweiger <SeSc838@users.noreply.github.com>
2021-01-21 16:54:16 +0100

    Base funtionality implemented

--- main.go
+++ main.go
@@ -1,5 +1,65 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+)
 
+func main() {
+	args := os.Args
+	var name string
+	var module bool = false
+	if len(args) < 2 {
+		name = "SomeGoProject"
+	} else {
+		name = args[1]
+	}
+	if len(args) > 2 {
+		if args[2] == "-m" {
+			module = true
+		}
+	}
+
+	infoDir, _ := os.Stat(name)
+
+	if infoDir == nil {
+		err := os.Mkdir(name, 0755)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		mainPath := name + "/main.go"
+
+		info, _ := os.Stat(mainPath)
+
+		if info == nil {
+			err2 := ioutil.WriteFile(mainPath, []byte("package main\n\nfunc main(){\n\n}"), 0755)
+			if err2 != nil {
+				log.Fatal(err2)
+				return
+			}
+		}
+
+		if module {
+
+			os.Chdir("./" + name)
+
+			cmd := exec.Command("go", "mod", "init")
+
+			if _, err := cmd.Output(); err != nil {
+				fmt.Println("Error: ", err)
+				return
+			}
+
+		}
+
+		fmt.Printf("Project %s was created.\n", name)
+
+	} else {
+		fmt.Printf("Project/Folder %s already exists.\n", name)
+	}
 }
